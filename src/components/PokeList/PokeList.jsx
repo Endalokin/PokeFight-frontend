77d7f837528d3b877754cf64865448b53f486332
Nora Westphal <tupfen@hotmail.de>
2023-12-01 14:29:02 +0100

    Pokemon in PoleList links to PokeDetail

--- src/components/PokeList/PokeList.jsx
+++ src/components/PokeList/PokeList.jsx
@@ -1,23 +1,15 @@
 import React, { useState } from 'react'
-import { Outlet } from 'react-router-dom'
+import { Outlet, Link } from 'react-router-dom'
 import FilterSlideOver from './FilterSlideOver'
 import { pokemons } from '../../utils/pokemons'
 
 export default function PokeList() {
 
     const [showOverlay, setShowOverlay] = useState("invisible")
-    const [typeFilter, setTypeFilter] = useState("")
-    const [detailPokemon, setDetailPokemon] = useState("Pikachu")
 
     function toggleFilterOverlay(e) {
         e.preventDefault()
         showOverlay == "" ? setShowOverlay("invisible") : setShowOverlay("")
-    }
-
-    function changeDetailPokemon(e) {
-        e.preventDefault()
-        console.log(`${detailPokemon} changes to ${e.target.id}`)
-        setDetailPokemon(e.target.id)
     }
 
     const [pokeList, setPokeList] = useState(pokemons)
@@ -33,7 +25,7 @@ export default function PokeList() {
                     <button onClick={toggleFilterOverlay}>Open Filter</button>
                     <div className="grid grid-cols-4">
                         {pokeList.map(p => {
-                            return <div id={p.name} onClick={changeDetailPokemon}>{p.name}</div>
+                            return <Link to={`pokemon/${p.name}`} style={{color: 'lightgrey'}} ><div id={p.name}>{p.name}</div></Link>
                         })}
                     </div>
                 </div>
